42df812718d2fd47e03c7a02ce86cf6671d31fbe
John ray D. Judaya <judayajohnray@gmail.com>
2026-02-19 22:25:15 +0800

    feat: new db

--- src/routes/(auth)/signin.tsx
+++ src/routes/(auth)/signin.tsx
@@ -53,7 +53,6 @@ function RouteComponent() {
             Sign in to your account
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
-            Or{" "}
             <Link to="/signup" className="font-medium text-blue-600 hover:text-blue-500">
               create a new account
             </Link>
