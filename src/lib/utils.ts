511cd3fb640d213b3746252412ce6f16d5e367fd
John ray D. Judaya <judayajohnray@gmail.com>
2026-04-03 01:36:44 +0800

    feat: add slug generator

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function generateSlug(name: string) {
+  const base = name
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-");
+
+  const suffix = Bun.randomUUIDv7().slice(0, 8);
+
+  return `${base}-${suffix}`;
+}
