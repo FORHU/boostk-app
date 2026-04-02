723ab10716c4486382263e959b4688324e489b76
John ray D. Judaya <judayajohnray@gmail.com>
2026-04-02 15:53:44 +0800

    fix: remove pg being called in client side

--- src/modules/project/project.queries.ts
+++ src/modules/project/project.queries.ts
@@ -1,6 +1,5 @@
 import { queryOptions } from "@tanstack/react-query";
 import { getOrgProjectsFn } from "@/modules/organization/organization.functions";
-import { getProjectById } from "@/modules/project/project.service";
 
 export const projectQueries = {
   all: ["projects"],
@@ -8,10 +7,5 @@ export const projectQueries = {
     queryOptions({
       queryKey: [...projectQueries.all, "by-org", organizationId],
       queryFn: () => getOrgProjectsFn({ data: { organizationId } }),
-    }),
-  byId: (id: string) =>
-    queryOptions({
-      queryKey: [...projectQueries.all, "by-id", id],
-      queryFn: () => getProjectById(id),
     }),
 };
