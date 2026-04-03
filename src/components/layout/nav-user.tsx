90592a2309f5bd6ab6708169c41fd52a60cbb78e
John ray D. Judaya <judayajohnray@gmail.com>
2026-04-03 15:47:04 +0800

    feat: move logout to side nav

--- src/components/layout/nav-user.tsx
+++ src/components/layout/nav-user.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
+import { useRouter } from "@tanstack/react-router";
 import { BadgeCheckIcon, BellIcon, ChevronsUpDownIcon, CreditCardIcon, LogOutIcon, SparklesIcon } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -12,24 +14,38 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar";
+import { authClient } from "@/lib/auth-client";
+import { authQueries } from "@/modules/auth/auth.queries";
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string;
-    email: string;
-    avatar: string;
-  };
-}) {
+export function NavUser() {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
   const { isMobile } = useSidebar();
+  // TODO: investigate why useSuspenseQuery is not working (marked return data as non-nullable)
+  const { data: authSession } = useSuspenseQuery(authQueries.authUser());
+
+  if (!authSession) {
+    console.error("No auth session. Redirecting to login.");
+    router.navigate({ to: "/signin" });
+    return null;
+  }
+
+  const { user } = authSession;
+
+  const handleLogout = async () => {
+    await authClient.signOut();
+    await queryClient.invalidateQueries();
+    router.navigate({ to: "/signin" });
+  };
+
   return (
     <SidebarMenu>
       <SidebarMenuItem>
         <DropdownMenu>
           <DropdownMenuTrigger render={<SidebarMenuButton size="lg" className="aria-expanded:bg-muted" />}>
             <Avatar>
-              <AvatarImage src={user.avatar} alt={user.name} />
+              <AvatarImage src={"/avatars/laugh-orange-cat.gif"} alt={user.name} />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             <div className="grid flex-1 text-left text-sm leading-tight">
@@ -48,8 +64,8 @@ export function NavUser({
               <DropdownMenuLabel className="p-0 font-normal">
                 <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                   <Avatar>
-                    <AvatarImage src={user.avatar} alt={user.name} />
-                    <AvatarFallback>CN</AvatarFallback>
+                    <AvatarImage src={"/avatars/laugh-orange-cat.gif"} alt={user.name} />
+                    <AvatarFallback>{user.name.slice(0, 2).toUpperCase()}</AvatarFallback>
                   </Avatar>
                   <div className="grid flex-1 text-left text-sm leading-tight">
                     <span className="truncate font-medium">{user.name}</span>
@@ -81,7 +97,7 @@ export function NavUser({
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={handleLogout}>
               <LogOutIcon />
               Log out
             </DropdownMenuItem>
