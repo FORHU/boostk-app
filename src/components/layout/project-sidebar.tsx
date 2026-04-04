bb7ec1cb8704b1618e02206296ab0f5037349efd
John ray D. Judaya <judayajohnray@gmail.com>
2026-04-04 11:53:50 +0800

    feat: implement project sidebar

---
+++ src/components/layout/project-sidebar.tsx
@@ -0,0 +1,73 @@
+import { Link } from "@tanstack/react-router";
+import { HatGlasses, Home, Settings, Ticket } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+interface ProjectSidebarProps extends React.ComponentProps<typeof Sidebar> {
+  projectId: string;
+}
+
+const routes = [
+  {
+    label: "Project Overview",
+    icon: Home,
+    href: "/dashboard/project/$projectId",
+  },
+  {
+    label: "Tickets",
+    icon: Ticket,
+    href: "/dashboard/project/$projectId/tickets",
+  },
+  {
+    label: "Agents",
+    icon: HatGlasses,
+    href: "/dashboard/project/$projectId/agents",
+  },
+  {
+    label: "Project Settings",
+    icon: Settings,
+    href: "/dashboard/project/$projectId/settings",
+  },
+];
+
+export default function ProjectSidebar({ projectId, ...props }: ProjectSidebarProps) {
+  return (
+    <Sidebar {...props}>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu className="flex flex-col gap-1">
+              {routes.map((route) => (
+                <Link
+                  key={route.href}
+                  to={route.href}
+                  preload="intent"
+                  params={{ projectId }}
+                  activeOptions={{ exact: true }}
+                  className="flex items-center gap-2"
+                  activeProps={{
+                    className: "bg-sidebar-accent text-sidebar-accent-foreground font-medium ",
+                  }}
+                >
+                  <SidebarMenuItem className="w-full">
+                    <SidebarMenuButton className="text-muted-foreground ">
+                      <route.icon className="size-4" />
+                      <span>{route.label}</span>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </Link>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
---
+++ src/routes/(app)/dashboard/project.$projectId/route.tsx
@@ -0,0 +1,43 @@
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+import ProjectSidebar from "@/components/layout/project-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { REDIRECT_REASON } from "@/enums/enums";
+import { getProjectFn } from "@/modules/project/project.functions";
+
+export const Route = createFileRoute("/(app)/dashboard/project/$projectId")({
+  beforeLoad: async ({ params }) => {
+    const project = await getProjectFn({ data: { projectId: params.projectId } });
+    if (!project) {
+      throw redirect({ to: "/dashboard/organizations", search: { reason: REDIRECT_REASON.PERMISSION_DENIED } });
+    }
+
+    return { project };
+  },
+  component: OrganizationLayout,
+});
+
+function OrganizationLayout() {
+  const { project } = Route.useRouteContext();
+
+  return (
+    <div className="flex-1 min-h-0 relative">
+      {/* sets the custom properties for the sidebar */}
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "13rem",
+            "--sidebar-width-icon": "3rem",
+            "--sidebar-offset": "2.75rem",
+          } as React.CSSProperties
+        }
+      >
+        <ProjectSidebar projectId={project.id} />
+        <SidebarInset>
+          <div className="flex-1 overflow-auto">
+            <Outlet />
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  );
+}
