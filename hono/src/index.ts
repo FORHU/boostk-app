f52ce47159eae27f22905e044bb0ab19e1e84f2e
John Ray D. Judaya <judayajohnray@gmail.com>
2026-04-06 22:38:04 +0800

    feat: add socket (socket io with hono)

---
+++ hono/src/index.ts
@@ -0,0 +1,43 @@
+import { Server as Engine } from "@socket.io/bun-engine";
+import { Server } from "socket.io";
+
+const io = new Server();
+const engine = new Engine({
+  path: "/socket.io/",
+  cors: {
+    origin: "*", 
+  }
+});
+
+io.bind(engine);
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room: ${room}`);
+  });
+
+  socket.on("leave_room", (room) => {
+    socket.leave(room);
+    console.log(`User ${socket.id} left room: ${room}`);
+  });
+
+  socket.on("new_message", (data: { room: string; messageId?: string }) => {
+    io.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("ticket_created", (data: { projectRoom: string }) => {
+    io.to(data.projectRoom).emit("ticket_list_updated", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
+
+export default {
+  port: 3001,
+  ...engine.handler(),
+};
