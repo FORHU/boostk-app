5cf4aa86e2dc3efcf5240da59bff089131d105e0
John Ray D. Judaya <judayajohnray@gmail.com>
2026-04-07 10:59:00 +0800

    fix: hardcoded url for testing

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.VITE_SOCKET_URL);
+export const socket = io("http://mksc888wwosk4gsksc4oskc4.158.178.241.113.sslip.io");
