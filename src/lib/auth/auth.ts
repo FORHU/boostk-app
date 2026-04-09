512f52307c172cf7f7728b30bf5f78bde5d491a8
John Ray D. Judaya <judayajohnray@gmail.com>
2026-04-09 14:57:29 +0800

    feat: add dynamic roles (better auth)

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { organization } from "better-auth/plugins";
+import { accessControl } from "@/lib/auth/permissions";
 import { prisma } from "@/lib/prisma";
 
 export const auth = betterAuth({
@@ -11,5 +12,12 @@ export const auth = betterAuth({
     enabled: true,
   },
   baseURL: process.env.BETTER_AUTH_URL,
-  plugins: [organization()],
+  plugins: [
+    organization({
+      accessControl,
+      dynamicAccessControl: {
+        enabled: true,
+      },
+    }),
+  ],
 });
