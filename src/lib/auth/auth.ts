9acce730d66338e6d9e85d5e661bbddedfd9e4ff
John Ray D. Judaya <judayajohnray@gmail.com>
2026-04-09 16:42:09 +0800

    chore: improve better auth bundle size

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,5 +1,5 @@
-import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { betterAuth } from "better-auth/minimal";
 import { organization } from "better-auth/plugins";
 import { accessControl } from "@/lib/auth/permissions";
 import { prisma } from "@/lib/prisma";
