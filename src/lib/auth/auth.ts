7d2b45f0104703b102e22893002ad026312aeea6
John Ray D. Judaya <judayajohnray@gmail.com>
2026-04-09 18:53:09 +0800

    feat: add better auth admin plugin

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,6 +1,6 @@
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { betterAuth } from "better-auth/minimal";
-import { organization } from "better-auth/plugins";
+import { admin, organization } from "better-auth/plugins";
 import { accessControl } from "@/lib/auth/permissions";
 import { prisma } from "@/lib/prisma";
 
@@ -13,6 +13,7 @@ export const auth = betterAuth({
   },
   baseURL: process.env.BETTER_AUTH_URL,
   plugins: [
+    admin(),
     organization({
       accessControl,
       dynamicAccessControl: {
