4466929046f9984e0a44025629b6ad7e5584dfd0
John Ray D. Judaya <judayajohnray@gmail.com>
2026-04-22 14:56:31 +0800

    chore: format and lint

--- src/routes/(app)/dashboard/(admin)/users.tsx
+++ src/routes/(app)/dashboard/(admin)/users.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/(app)/dashboard/(admin)/users')({
+export const Route = createFileRoute("/(app)/dashboard/(admin)/users")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/(app)/dashboard/(admin)/users"!</div>
+  return <div>Hello "/(app)/dashboard/(admin)/users"!</div>;
 }
